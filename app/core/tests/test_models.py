4f6f92a3dd0653b2cc8343a5dce1e783a036b399
hosseinhtt <hossein.taybi@yahoo.com>
2023-10-13 10:51:42 +0330

    add recipe feature

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -56,3 +56,13 @@ class ModelTests(TestCase):
             name='Cucumber'
         )
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """test the string recipe representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='steak and mushroom sauce',
+            time_minutes=5,
+            price=5.00
+        )
+        self.assertEqual(str(recipe), recipe.title)
